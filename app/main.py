296d5980e8f2db10a565a23ac0a5d9083efee0ca
Parham <parhamrouzbeh@yahoo.com>
2021-12-17 12:17:17 +0330

    broke some stuff on purpose

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World, successfully deployed from CI/CD pipline"}
+    return {"message": "Hello World"}
 
 # uvicorn main:app --reload
 # uvicorn app.main:app --reload
